8820c8762218a76cb02f4ae2c4f8b628139b5e71
Jevin Maltais <jevyjevjevs@gmail.com>
2016-02-05 12:10:28 -0500

    Working

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # turnkey-botkit
 A template project to get you started writing Botkit bots for Slack
+
+To start
+```
+npm install
+npm install localtunnel
+lt --port 8765 --subdomain happinessbot
+
+clientId=XYZ clientSecret=ABC PORT=8765 npm start
+```
--- index.js
+++ index.js
@@ -62,7 +62,7 @@ controller.on('create_bot',function(bot,config) {
           console.log(err);
         } else {
           convo.say('I am a bot that has just joined your team');
-          convo.say('You must now /invite me to a channel so that I can be of use!');
+          convo.say('If you ever have workplace issues, let me know!  I will find someone to help you out!');
         }
       });
 
